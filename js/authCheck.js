cac8a08ca505e6bd5d2f8b74a7a34420ad04bffa
Nelkeys <nelsonekoh212@gmail.com>
2024-12-19 02:49:14 +0100

    try

--- js/authCheck.js
+++ js/authCheck.js
@@ -1,10 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-app.js";
 import {
   getAuth,
   onAuthStateChanged,
 } from "https://www.gstatic.com/firebasejs/9.21.0/firebase-auth.js";
 
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBQ4vGjiLjaAG5kIP-IWab441RETilwdks",
+  authDomain: "nacos-app.firebaseapp.com",
+  databaseURL: "https://nacos-app-default-rtdb.firebaseio.com",
+  projectId: "nacos-app",
+  storageBucket: "nacos-app.firebasestorage.app",
+  messagingSenderId: "982514858036",
+  appId: "1:982514858036:web:66e7bb244b772ce65d7261", 
+};
+
 // Initialize Firebase
-const auth = getAuth();
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 // Check if the user is logged in
 onAuthStateChanged(auth, (user) => {
